9d9748c1bb9975aed83af4b5fe606ff4ba7c91a6
Jaesung Jung <jaesung.jung1986@gmail.com>
2026-04-28 19:11:33 +0900

    Initial commit

---
+++ SimControl/SimControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimControlApp.swift
+//  SimControl
+//
+//  Created by 정재성 on 4/28/26.
+//
+
+import SwiftUI
+
+@main
+struct SimControlApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
