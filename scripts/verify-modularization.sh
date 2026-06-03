39af6f146bfc2abfc378c2b896094b72d4ece7f5
Jaesung Jung <jaesung.jung1986@gmail.com>
2026-06-04 06:35:37 +0900

    refactor: split main window developer tool tests

--- scripts/verify-modularization.sh
+++ scripts/verify-modularization.sh
@@ -354,8 +354,8 @@ assert_max_lines \
   "$PACKAGE_SOURCES/WorkspaceFeature/WorkspaceFeature.swift"
 
 assert_max_lines \
-  "MainWindowFeatureTests.swift는 root feature 테스트를 기능별 파일로 분리해 1000줄 이하로 유지합니다." \
-  1000 \
+  "MainWindowFeatureTests.swift는 root feature 테스트를 기능별 파일로 분리해 500줄 이하로 유지합니다." \
+  500 \
   "$PACKAGE_TESTS/MainWindowFeatureTests/MainWindowFeatureTests.swift"
 
 assert_no_app_swift_sources_outside_app
